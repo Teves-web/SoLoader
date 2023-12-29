fe7d290b504d0de5473de3737ec881280eddf984
João Teves <68256316+Teves-web@users.noreply.github.com>
2023-12-29 10:02:46 -0300

    Update SoSource.java

--- java/com/facebook/soloader/SoSource.java
+++ java/com/facebook/soloader/SoSource.java
@@ -17,138 +17,63 @@
 package com.facebook.soloader;
 
 import android.os.StrictMode;
+
+import javax.annotation.Nullable;
 import java.io.File;
 import java.io.IOException;
 import java.util.Collection;
-import javax.annotation.Nullable;
 
+/**
+ * Abstract class representing a source of shared libraries.
+ */
 public abstract class SoSource {
 
-  /** This SoSource doesn't know how to provide the given library. */
-  public static final int LOAD_RESULT_NOT_FOUND = 0;
+    public static final int LOAD_RESULT_NOT_FOUND = 0;
+    public static final int LOAD_RESULT_LOADED = 1;
+    public static final int LOAD_RESULT_IMPLICITLY_PROVIDED = 2;
+    public static final int LOAD_FLAG_ALLOW_IMPLICIT_PROVISION = 1;
+    @Deprecated public static final int LOAD_FLAG_ALLOW_SOURCE_CHANGE = 1 << 1;
+    public static final int LOAD_FLAG_MIN_CUSTOM_FLAG = 1 << 2;
+    public static final int PREPARE_FLAG_ALLOW_ASYNC_INIT = (1 << 0);
+    public static final int PREPARE_FLAG_FORCE_REFRESH = (1 << 1);
+    public static final int PREPARE_FLAG_DISABLE_FS_SYNC_JOB = (1 << 2);
 
-  /** This SoSource loaded the given library. */
-  public static final int LOAD_RESULT_LOADED = 1;
+    protected void prepare(int flags) throws IOException {
+        /* By default, do nothing */
+    }
 
-  /**
-   * This SoSource did not load the library, but verified that the system loader will load it if
-   * some other library depends on it. Returned only if LOAD_FLAG_ALLOW_IMPLICIT_PROVISION is
-   * provided to loadLibrary.
-   */
-  public static final int LOAD_RESULT_IMPLICITLY_PROVIDED = 2;
+    public abstract int loadLibrary(String soName, int loadFlags, StrictMode.ThreadPolicy threadPolicy) throws IOException;
 
-  /** Allow loadLibrary to implicitly provide the library instead of actually loading it. */
-  public static final int LOAD_FLAG_ALLOW_IMPLICIT_PROVISION = 1;
+    @Nullable
+    public abstract File unpackLibrary(String soName) throws IOException;
 
-  /** Allow loadLibrary to reparse the so sources directories. */
-  @Deprecated public static final int LOAD_FLAG_ALLOW_SOURCE_CHANGE = 1 << 1;
+    @Nullable
+    protected File getSoFileByName(String soName) throws IOException {
+        return null;
+    }
 
-  /**
-   * Min flag that can be used in customized {@link SoFileLoader#load(String, int)} implementation.
-   * The custom flag value has to be greater than this.
-   */
-  public static final int LOAD_FLAG_MIN_CUSTOM_FLAG = 1 << 2;
+    @Nullable
+    public String getLibraryPath(String soFileName) throws IOException {
+        return null;
+    }
 
-  /** Allow prepare to spawn threads to do background work. */
-  public static final int PREPARE_FLAG_ALLOW_ASYNC_INIT = (1 << 0);
+    @Nullable
+    public String[] getLibraryDependencies(String soName) throws IOException {
+        return null;
+    }
 
-  /** Force prepare to refresh libs. */
-  public static final int PREPARE_FLAG_FORCE_REFRESH = (1 << 1);
+    public void addToLdLibraryPath(Collection<String> paths) {
+        /* By default, do nothing */
+    }
 
-  /** Disable so file fsync job. */
-  public static final int PREPARE_FLAG_DISABLE_FS_SYNC_JOB = (1 << 2);
+    public String[] getSoSourceAbis() {
+        return SysUtil.getSupportedAbis();
+    }
 
-  /** Prepare to install this SoSource in SoLoader. */
-  protected void prepare(int flags) throws IOException {
-    /* By default, do nothing */
-  }
+    public abstract String getName();
 
-  /**
-   * Load a shared library library into this process. This routine is independent of {@link
-   * #loadLibrary}.
-   *
-   * @param soName Name of library to load
-   * @param loadFlags Zero or more of the LOAD_FLAG_XXX constants.
-   * @param threadPolicy Strict Mode policy
-   * @return One of the LOAD_RESULT_XXX constants.
-   * @throws IOException IOException
-   */
-  public abstract int loadLibrary(
-      String soName, int loadFlags, StrictMode.ThreadPolicy threadPolicy) throws IOException;
-
-  /**
-   * Ensure that a shared library exists on disk somewhere. This routine is independent of {@link
-   * #loadLibrary}.
-   *
-   * @param soName Name of library to load
-   * @return File if library found; {@code null} if not.
-   * @throws IOException IOException
-   */
-  @Nullable
-  public abstract File unpackLibrary(String soName) throws IOException;
-
-  /**
-   * Returns the so file for the specified library. Returns null if the library does not exist or if
-   * it's not backed by a file.
-   */
-  @Nullable
-  protected File getSoFileByName(String soName) throws IOException {
-    return null;
-  }
-
-  /**
-   * Gets the full path of a library if it is found on this SoSource.
-   *
-   * @param soFileName the full file name of the library
-   * @return the full path of a library if it is found on this SoSource, null otherwise.
-   * @throws IOException if there is an error calculating {@code soFileName}'s canonical path
-   */
-  @Nullable
-  public String getLibraryPath(String soFileName) throws IOException {
-    return null;
-  }
-
-  /**
-   * Gets the dependencies of a library if it is found on this SoSource
-   *
-   * @param soName Name of library to inspect
-   * @return An array of library names upon which {@code soName} needs for linking
-   * @throws IOException if {@code soName} is found but there is an error reading it
-   */
-  @Nullable
-  public String[] getLibraryDependencies(String soName) throws IOException {
-    return null;
-  }
-
-  /**
-   * Add an element to an LD_LIBRARY_PATH under construction.
-   *
-   * @param paths Collection of paths to which to add
-   */
-  public void addToLdLibraryPath(Collection<String> paths) {
-    /* By default, do nothing */
-  }
-
-  /**
-   * Return an array of ABIs handled by this SoSource.
-   *
-   * @return ABIs supported by this SoSource
-   */
-  public String[] getSoSourceAbis() {
-    /* By default, the same as the device */
-    return SysUtil.getSupportedAbis();
-  }
-
-  /** @return the name of this SoSource for logging purposes */
-  public abstract String getName();
-
-  /**
-   * Return the class name of the actual instance. Useful for debugging.
-   *
-   * @return the instance class name
-   */
-  @Override
-  public String toString() {
-    return getName();
-  }
+    @Override
+    public String toString() {
+        return getName();
+    }
 }
