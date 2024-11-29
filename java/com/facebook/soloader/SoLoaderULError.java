564c78b09d1feaf3a56013b4483ff33902ea29bb
João Teves <68256316+Teves-web@users.noreply.github.com>
2024-11-28 21:15:14 -0300

    Update SoLoaderULError.java

--- java/com/facebook/soloader/SoLoaderULError.java
+++ java/com/facebook/soloader/SoLoaderULError.java
@@ -16,22 +16,43 @@
 
 package com.facebook.soloader;
 
+import androidx.annotation.NonNull;
+
+/**
+ * Custom exception to handle errors related to loading shared objects (SO files).
+ */
 @DoNotStripAny
 public class SoLoaderULError extends UnsatisfiedLinkError {
 
-  private String mSoName;
+    private final String soName;
 
-  public SoLoaderULError(String soName, String error) {
-    super(error);
-    mSoName = soName;
-  }
+    /**
+     * Constructs a new {@code SoLoaderULError} with the specified shared object name and error message.
+     *
+     * @param soName the name of the shared object that caused the error.
+     * @param error the detailed error message.
+     */
+    public SoLoaderULError(@NonNull String soName, @NonNull String error) {
+        super(error);
+        this.soName = soName;
+    }
 
-  public SoLoaderULError(String soName) {
-    super();
-    mSoName = soName;
-  }
+    /**
+     * Constructs a new {@code SoLoaderULError} with the specified shared object name.
+     *
+     * @param soName the name of the shared object that caused the error.
+     */
+    public SoLoaderULError(@NonNull String soName) {
+        this.soName = soName;
+    }
 
-  public String getSoName() {
-    return mSoName;
-  }
+    /**
+     * Retrieves the name of the shared object that caused the error.
+     *
+     * @return the shared object name.
+     */
+    @NonNull
+    public String getSoName() {
+        return soName;
+    }
 }
